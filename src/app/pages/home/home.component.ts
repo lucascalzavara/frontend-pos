80f5bc6b68c3a9b148d7e4b13aa10051b4bb0018
lucasftassi <lucasftassi@outlook.com>
2021-11-22 23:40:45 -0300

    ajuste

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { DATE_PIPE_DEFAULT_TIMEZONE } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
 
@@ -13,13 +14,14 @@ export class HomeComponent implements OnInit {
   imagem_original: string = "https://img.pebmed.com.br/wp-content/uploads/2019/11/29111226/dengue-1.jpg.webp";
   imagem_processada: string = "https://img.pebmed.com.br/wp-content/uploads/2019/11/29111226/dengue-1.jpg.webp";
   quantidade_ovos: number = 300;
+  data_hora: any = null;
 
   ngOnInit(): void {
   }
 
   capturar() {
     this.httpService.capturarEProcessar().subscribe(res => {
-
+      this.data_hora = new Date();
     });
   }
 
